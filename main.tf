84d3cd718fb0527c377ce1d44089ed503cef56e5
Jonathan Ballet <jballet@edgelab.ch>
2021-08-18 10:14:04 +0200

    fix: output the certificate once it has been validated (#1)
    Output the certificate ARN only when it has been validated, otherwise
    we may try to use it although it's still in a pending state, which doesn't
    work for other resources (like ALB listeners).

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 # Request a certificate for the specified domain name.
-resource aws_acm_certificate "this" {
+resource "aws_acm_certificate" "this" {
   domain_name       = var.domain_name
   validation_method = "DNS"
 
@@ -19,7 +19,7 @@ locals {
 }
 
 # Register records to prove we own the domain name
-resource aws_route53_record "verify" {
+resource "aws_route53_record" "verify" {
   # Following this
   # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/version-3-upgrade#resource-aws_acm_certificate
   # I would expect this to work:
@@ -47,12 +47,20 @@ resource aws_route53_record "verify" {
 }
 
 # Wait for the certificate to be issued
-resource aws_acm_certificate_validation "this" {
+resource "aws_acm_certificate_validation" "this" {
   certificate_arn = aws_acm_certificate.this.arn
   # validation_record_fqdns = [for record in aws_route53_record.verify : record.fqdn]
   validation_record_fqdns = aws_route53_record.verify[*].fqdn
 }
 
 output "arn" {
-  value = aws_acm_certificate.this.arn
+  # Output the certificate only once it has been validated.
+  #
+  # Otherwise, Terraform may try to feed the certificate ARN to another
+  # resource (such as a load-balancer listener), which may be rejected because
+  # the certificate is not valid:
+  #
+  #   UnsupportedCertificate: The certificate 'XXX' must have a fully-qualified
+  #   domain name, a supported signature, and a supported key size.
+  value = aws_acm_certificate_validation.this.certificate_arn
 }
